dab57d4179c5c582803bba785d04c4e4480d6949
jvv00 <jvv0006@gmail.com>
2024-02-07 17:31:49 +0900

    헤더 영역 완성하기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import Routers from './Routers'
-import { ChakraProvider,} from '@chakra-ui/react'
-import theme from './theme/theme';
-import { ThemeProvider } from 'styled-components';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-gsap.registerPlugin (gsap/ScrollTrigger);
+import Home from './views/Home'
+import Today from './views/Today'
+import Developer from './views/Developer'
+import Webd from './views/Webd'
+import Website from './views/Website'
+import Gsap from './views/Gsap'
+import Port from './views/Port'
+import Youtube from './views/Youtube'
+import Channel from './views/Channel'
+import Video from './views/Video'
+import Search from './views/Search'
+import Not from './views/Not'
 
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <Routers />
-      </ThemeProvider>
-    </ChakraProvider>
-    
-  )
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/today' element={<Today />} />
+                <Route path='/developer' element={<Developer />} />
+                <Route path='/webd' element={<Webd />} />
+                <Route path='/website' element={<Website />} />
+                <Route path='/gsap' element={<Gsap />} />
+                <Route path='/port' element={<Port />} />
+                <Route path='/youtube' element={<Youtube />} />
+                <Route path='/channel/:channelId' element={<Channel />} />
+                <Route path='/video/:videoId' element={<Video />} />
+                <Route path='/search/:searchId' element={<Search />} />
+                <Route path='*' element={<Not />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
